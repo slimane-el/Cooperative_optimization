66e2622c37dcb7b11bb33fc1d0597b47289679fd
Soulaimane El Mennaoui <soulaimane.el-mennaoui@ensta-paris.fr>
2024-03-05 16:01:00 +0100

    oui oui

--- DGD.py
+++ DGD.py
@@ -4,3 +4,14 @@ import pickle
 import cvxpy as cp
 import networkx as nx
 from utils import kernel_matrix, kernel_im
+
+
+def DGD_revisited(x, mu, sigma, a, adjacency_matrix):
+    # stacked points
+    y_0 = x
+    n = len(x)
+    W = 1/a*(adjacency_matrix)  # define the weight matrix
+    # define the kronecker product of the weight matrix
+    W_bar = np.kron(W, np.eye(n))
+
+    return alpha_optim
--- Main.py
+++ Main.py
@@ -1,9 +1,4 @@
-import numpy as np
-import matplotlib.pyplot as plt
-import pickle
-import cvxpy as cp
-import networkx as nx
-from utils import kernel_matrix, kernel_im
+ 
 
 
 # Load the data
