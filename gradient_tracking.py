f421f8551b00c06a80be496b7ff1c208793e4fc6
Matis Nanni <matis@8octobre.net>
2024-03-06 12:35:13 +0100

    gt

--- gradient_tracking.py
+++ gradient_tracking.py
@@ -1,5 +1,65 @@
 from utils import *
 
+def gradient_tracking(x, y, x_selected, m, sigma, mu, lr):
+    """
+    This function implements the gradient tracking algorithm.
+
+    Parameters
+    ----------
+    x : list of numpy array
+        The x coordinates of the data points for each agent.
+    y : list of numpy array
+        The y coordinates of the data points for each agent.
+    n : int
+        The number of data points.
+    m : int
+        The number of selected data points.
+    sigma : float
+        The kernel parameter.
+    lr : float
+        The learning rate.
+    alpha_optim : numpy array
+        The optimal alpha.
+
+    Returns
+    -------
+    alpha : numpy array
+        The final alpha.
+
+    """
+    a = len(x) # number of agents
+    # stacked points
+    alpha = [np.arange(m) for i in range(a)] # initial alpha for each agent
+    gradient = [np.arange(m) for i in range(a)] # initial gradient for each agent
+    alpha = np.array(alpha).reshape(a*m, 1)
+    gradient = np.array(gradient).reshape(a*m, 1)
+    print(f'alpha shape : {alpha.shape}')
+    print(f'gradient shape : {gradient.shape}')
+
+    W = 1/a*(np.ones((a, a)))  # define the weight matrix
+    # define the kronecker product of the weight matrix
+    W_bar = np.kron(W, np.eye(m))
+    print(f'W_bar shape : {W_bar.shape}')
+    print(f'W_bar * alpha shape : {(W_bar @ alpha).shape}')
+
+    print()
+    alpha_mean = 1000*np.ones(m)
+    j = 0
+    while np.linalg.norm(alpha.reshape(a, m)[0] - alpha_mean) > 0.001 and j<1000:
+        j += 1
+        # for i in range(a):
+        #     alpha[i] = W_bar * alpha[i]+ lr * gradient[i]
+        #     gradient[i] = grad_alpha(x, y, sigma, alpha[i])
+        alpha_new = W_bar @ alpha - lr * gradient
+        # TODO alpha new sous forme de liste ??
+        gradient = W_bar @ gradient + grad_alpha(sigma, mu, y, x, x_selected, alpha_new) - \
+            grad_alpha(sigma, mu, y, x, x_selected, alpha)
+        alpha = alpha_new
+        alpha_mean = np.mean(alpha.reshape(a, m), axis=1)
+        print(f'alpha mean : {alpha_mean}')
+
+    return alpha
+
 if __name__ == "__main__":
     
     # # Load the data x and y
@@ -22,6 +82,12 @@ if __name__ == "__main__":
     print(type(x_selected))
     print(x_selected.shape)
     print(x_selected)
+
+    # Compute the alpha optimal
+    sigma = 0.5
+    mu = 0 
+    lr = 0.1
+    alpha_optim = gradient_tracking(agent_x, agent_y, x_selected, m, sigma, mu, lr)
 
     
     """
